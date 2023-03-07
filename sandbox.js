cdc3e08df9e91dcf3c1aab3f7160dcee96424e86
Mel B <72110634+professionalmel@users.noreply.github.com>
2023-03-07 11:58:50 -0800

    first commit

---
+++ sandbox.js
@@ -0,0 +1,39 @@
+// add to html 
+
+// <!-- <img class='icon' src="./assets/ideas.svg" alt="ideas"> -->
+// <!-- <div>
+//     <h2 onclick="show_()">Projects </h2>
+//     <h3 class="projects" >Boss Lady: Habbit Tracker</h3>
+//     <h3 class="projects">Cute Overload</h3>
+// </div> -->
+
+// add to javascript
+
+// let project_div= document.getElementsByClassName('projects')
+
+// for(let i = 0; i<project_div.length; i++){
+//     project_div[i].style.display="none"}
+
+
+// function show_(){
+//     console.log('click')
+//     for(let i = 0; i<project_div.length; i++){
+
+//         console.log(project_div[i])
+
+//         // let get_att = project_div[i].getAttribute('style')
+//         // console.log(get_att)
+        
+//         console.log(project_div[i].style.display)
+//         if(project_div[i].style.display == "none"){
+//             project_div[i].style.display = 'block';
+//             console.log('if');
+//         }
+
+//         else{
+//             project_div[i].style.display=='block';
+//             project_div[i].style.display= 'none'
+//             console.log('else')
+//         }
+//     }
+// }
